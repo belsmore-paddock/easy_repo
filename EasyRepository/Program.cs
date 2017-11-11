883dddca43986bcdd32274fb7dff3de76a32524f
Brad Elsmore <belsmore@gmail.com>
2017-11-10 20:04:29 -0500

    Add project files.

---
+++ EasyRepository/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EasyRepository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
